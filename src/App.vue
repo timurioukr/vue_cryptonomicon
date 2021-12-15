9a4798d5da76c0963e26690a3a1f6be290ba4334
Timur <timurioukr@gmail.com>
2021-12-15 19:09:50 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -105,7 +105,7 @@
           </div>
         </div>
         <button
-          v-on:keydown.enter="add"
+          v-on:enter="add"
           @click="add"
           type="button"
           class="
@@ -149,77 +149,85 @@
       </section>
 
       <div v-if="tickers.length">
-      <hr class="w-full border-t border-gray-600 my-4"/>
-      <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-        <div
-          v-for="t in tickers"
-          :key="t.name"
-          class="
-            bg-white
-            overflow-hidden
-            shadow
-            rounded-lg
-            border-purple-800 border-solid
-            cursor-pointer
-          "
-        >
-          <div class="px-4 py-5 sm:p-6 text-center">
-            <dt class="text-sm font-medium text-gray-500 truncate">
-              {{ t.name }} - USD
-            </dt>
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">{{ t.price }}</dd>
-          </div>
-          <div class="w-full border-t border-gray-200"></div>
-          <button
-            @click="handleDelete(t)"
+        <hr class="w-full border-t border-gray-600 my-4" />
+        <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+          <div
+            @click="select(t)"
+            v-for="t in tickers"
+            :key="t.name"
+            :class="sell === t ? 'border-4' : ''"
             class="
-              flex
-              items-center
-              justify-center
-              font-medium
-              w-full
-              bg-gray-100
-              px-4
-              py-4
-              sm:px-6
-              text-md text-gray-500
-              hover:text-gray-600 hover:bg-gray-200 hover:opacity-20
-              transition-all
-              focus:outline-none
+              bg-white
+              overflow-hidden
+              shadow
+              rounded-lg
+              border-purple-800 border-solid
+              cursor-pointer
             "
           >
-            <svg
-              class="h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#718096"
-              aria-hidden="true"
+            <div class="px-4 py-5 sm:p-6 text-center">
+              <dt class="text-sm font-medium text-gray-500 truncate">
+                {{ t.name }} - USD
+              </dt>
+              <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                {{ t.price }}
+              </dd>
+            </div>
+            <div class="w-full border-t border-gray-200"></div>
+            <button
+              @click.stop="handleDelete(t)"
+              class="
+                flex
+                items-center
+                justify-center
+                font-medium
+                w-full
+                bg-gray-100
+                px-4
+                py-4
+                sm:px-6
+                text-md text-gray-500
+                hover:text-gray-600 hover:bg-gray-200 hover:opacity-20
+                transition-all
+                focus:outline-none
+              "
             >
-              <path
-                fill-rule="evenodd"
-                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                clip-rule="evenodd"
-              ></path></svg
-            >Удалить
-          </button>
-        </div>
-        
-        
-        
-      </dl>
-      <hr class="w-full border-t border-gray-600 my-4" />
-    </div>
-      <section class="relative">
+              <svg
+                class="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="#718096"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                  clip-rule="evenodd"
+                ></path></svg
+              >Удалить
+            </button>
+          </div>
+        </dl>
+        <hr class="w-full border-t border-gray-600 my-4" />
+      </div>
+      <section v-if="sell" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          VUE - USD
+          {{ sell.name }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
-          <div class="bg-purple-800 border w-10 h-24"></div>
-          <div class="bg-purple-800 border w-10 h-32"></div>
-          <div class="bg-purple-800 border w-10 h-48"></div>
-          <div class="bg-purple-800 border w-10 h-16"></div>
+          <div
+            v-for="(bar, idx) in normalizeGraph()"
+            :key="idx"
+            :style="{ height: `${bar}%` }"
+            class="bg-purple-800 border w-10">
+          </div>
         </div>
-        <button type="button" class="absolute top-0 right-0">
+        
+        <button
+          @click="sell = null"
+          type="button"
+          class="absolute top-0 right-0"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -253,29 +261,49 @@ export default {
   components: {},
   data() {
     return {
-      ticker: 'default',
-      tickers: [
-        { name: 'DEMO', price: '-'},
-        { name: 'DEMO', price: '-'},
-        { name: 'DEMO', price: '-'},
-        ]
-    }
+      ticker: "",
+      tickers: [],
+      sell: '',
+      graph: []
+    };
   },
   methods: {
     add() {
-      const newTicker = {
+      const currentTicker = {
         name: this.ticker,
-        price: '-'
-      }
-      this.tickers.push(newTicker)
-      this.ticker = ''
+        price: "-",
+      };
+      this.tickers.push(currentTicker);
+      setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=131cdaabbbdd3b9303bb5df4616295000ddc0622a8a3b51b869b9276c30dc9cf`
+        );
+        const data = await f.json();
+        this.tickers.find((t) => t.name === currentTicker.name).price =
+          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+        if(this.sell.name === currentTicker.name) {
+          this.graph.push(data.USD)
+          // console.log("sssss",  this.graph)
+        }
+      }, 3000);
+      this.ticker = "";
     },
     handleDelete(tickerToRemove) {
-    this.tickers = this.tickers.filter(t => t !== tickerToRemove)
-  }
+      this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
+    },
+    normalizeGraph() {
+      const maxValue = Math.max(...this.graph);
+      const minValue = Math.min(...this.graph);
+      return this.graph.map(
+        price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      )
+    },
+    select(ticker) {
+      this.sell = ticker
+      this.graph = []
+    }
   },
 };
 </script>
 
-<style src="./app.css">
-</style>
+<style src="./app.css"></style>
